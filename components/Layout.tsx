ba3148966fea67d5d0cc12bba8b8704381cf5718
nwesthoff <nilsepils94@hotmail.com>
2020-05-07 01:00:12 +0200

    💅 Adds Roboto Font

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -23,6 +23,10 @@ const Layout: React.FunctionComponent<Props> = ({
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+      />
     </Head>
     {children}
   </div>
