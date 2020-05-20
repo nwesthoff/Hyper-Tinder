ae161c386425b1e4848ac3135c997947229a47cd
nwesthoff <nilsepils94@hotmail.com>
2020-05-20 21:51:11 +0200

    ✨ shuffle the cards so everyone gets a fair shot 🔀

--- components/HyperTinder.tsx
+++ components/HyperTinder.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import { useEffect, useState, Fragment } from "react";
 import TinderCard from "react-tinder-card";
 import HyperCard from "./HyperCard";
 import { User } from "../interfaces";
@@ -20,30 +20,61 @@ type Props = {
 };
 
 const HyperTinder = ({ users }: Props) => {
-  const onSwipe = (direction, user) => {
+  const [shuffleHash, setShuffleHash] = useState(undefined);
+
+  function shuffle(array: number[]) {
+    var currentIndex = array.length,
+      temporaryValue: number,
+      randomIndex: number;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
+
+  const onSwipe = (direction: string, user: { id?: number; fields: any }) => {
     if (direction === "right") {
       window.location.href = `mailto:${user?.fields?.email}`;
     }
     console.log("You swiped " + user?.fields?.firstName + ": " + direction);
   };
 
+  if (!shuffleHash && users?.length > 0) {
+    setShuffleHash(shuffle(users.map((user) => user.id)));
+  }
+
   return (
-    <React.Fragment>
-      {users?.length > 0 &&
-        users.map((user) => (
-          <StyledTinderCard
-            rotate={(Math.random() * 5 - 2.5).toFixed(0)}
-            translateX={(Math.random() * 20 - 10).toFixed(0)}
-            translateY={(Math.random() * 20 - 10).toFixed(0)}
-            key={user.id}
-            onCardLeftScreen={() => {}}
-            onSwipe={(dir) => onSwipe(dir, user)}
-            preventSwipe={["up", "down"]}
-          >
-            <HyperCard user={user} />
-          </StyledTinderCard>
-        ))}
-    </React.Fragment>
+    <Fragment>
+      {shuffleHash &&
+        users?.length > 0 &&
+        users
+          .sort((a, b) => {
+            return shuffleHash.indexOf(a.id) - shuffleHash.indexOf(b.id);
+          })
+          .map((user) => (
+            <StyledTinderCard
+              rotate={(Math.random() * 5 - 2.5).toFixed(0)}
+              translateX={(Math.random() * 20 - 10).toFixed(0)}
+              translateY={(Math.random() * 20 - 10).toFixed(0)}
+              key={user.id}
+              onCardLeftScreen={() => {}}
+              onSwipe={(dir) => onSwipe(dir, user)}
+              preventSwipe={["up", "down"]}
+            >
+              <HyperCard user={user} />
+            </StyledTinderCard>
+          ))}
+    </Fragment>
   );
 };
 
--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -1,6 +1,5 @@
 export const fetcher = async (url: string) => {
-  const response = await fetch(url)
-  const data = await response.json()
-  console.log(data)
-  return data
-}
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
